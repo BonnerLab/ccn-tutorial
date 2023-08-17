11fc82d56dc48b6aa10c3cf7c5d543c6e5d80581
Raj Magesh Gauthaman <raj@raj-magesh.org>
2023-08-17 04:42:59 -0400

    Built site for gh-pages

---
+++ site_libs/quarto-contrib/add-code-files-1.0.0/add-code-files.js
@@ -0,0 +1,6 @@
+// Changing Code summary text
+document.querySelectorAll('div[data-code-filename]').forEach(div => {
+  let filename = div.getAttribute('data-code-filename');
+  let summary = div.querySelector('summary');
+  summary.innerText = filename;
+})
